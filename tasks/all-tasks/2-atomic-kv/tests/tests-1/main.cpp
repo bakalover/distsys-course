290b67c6a1ba4e8cdf2cf74a7516e6d9b5cd89dd
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-09-25 01:07:45 +0300

    more simulations without adversaries

--- tasks/all-tasks/2-atomic-kv/tests/tests-1/main.cpp
+++ tasks/all-tasks/2-atomic-kv/tests/tests-1/main.cpp
@@ -224,18 +224,20 @@ size_t RunSimulation(size_t seed) {
   // Adversaries
 
   if (random.Maybe(3)) {
-    // Network partitions
-    world.AddAdversary(NetAdversary);
-  }
+    if (random.Maybe(3)) {
+      // Network partitions
+      world.AddAdversary(NetAdversary);
+    }
 
-  if (random.Maybe(7)) {
-    // Reboots, pauses
-    world.AddAdversary(NodeAdversary);
-  }
+    if (random.Maybe(7)) {
+      // Reboots, pauses
+      world.AddAdversary(NodeAdversary);
+    }
 
-  if (random.Maybe(11)) {
-    // Crashes
-    world.AddAdversary(NodeReeper);
+    if (random.Maybe(11)) {
+      // Crashes
+      world.AddAdversary(NodeReeper);
+    }
   }
 
   // Log file
