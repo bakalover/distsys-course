16aeb6c557542c25477b58bb1c866dd596874607
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-11-04 13:08:25 +0300

    adapt to persist lib

--- tasks/all-tasks/4-rsm-multipaxos/rsm/replica/store/log.cpp
+++ tasks/all-tasks/4-rsm-multipaxos/rsm/replica/store/log.cpp
@@ -1,6 +1,7 @@
 #include <rsm/replica/store/log.hpp>
 
-#include <persist/rsm/log/file.hpp>
+#include <persist/rsm/multipaxos/log/file.hpp>
+#include <persist/rsm/multipaxos/log/segmented/log.hpp>
 
 #include <muesli/serialize.hpp>
 
@@ -8,37 +9,39 @@
 
 namespace rsm {
 
-Log::Log(const persist::fs::Path& dir) : impl_(MakeLogImpl(dir)) {
+Log::Log(const persist::fs::Path& store_dir) : impl_(MakeLogImpl(store_dir)) {
 }
 
 void Log::Open() {
   impl_->Open();
 }
 
+bool Log::IsEmpty(size_t index) const {
+  return impl_->IsEmpty(index);
+}
+
 std::optional<LogEntry> Log::Read(size_t index) const {
-  auto entry = impl_->Read(index);
+  auto entry = impl_->TryRead(index);
   if (entry.has_value()) {
     return muesli::Deserialize<LogEntry>(*entry);
   }
   return std::nullopt;
 }
 
-void Log::Update(size_t index, LogEntry entry) {
+void Log::Update(size_t index, const LogEntry& entry) {
   impl_->Update(index, muesli::Serialize(entry));
-}
-
-bool Log::IsEmpty(size_t index) const {
-  return !impl_->Read(index).has_value();
 }
 
 void Log::TruncatePrefix(size_t end_index) {
   impl_->TruncatePrefix(end_index);
 }
 
-std::shared_ptr<persist::rsm::ILog> Log::MakeLogImpl(
-    const persist::fs::Path& dir) {
-  return std::make_shared<persist::rsm::FileLog>(whirl::node::rt::Fs(),
-                                                 dir / "log");
+std::shared_ptr<Log::ILogImpl> Log::MakeLogImpl(
+    const persist::fs::Path& store_dir) {
+  const auto log_path = store_dir / "log";
+
+  return std::make_shared<persist::rsm::multipaxos::FileLog>(
+      whirl::node::rt::Fs(), log_path);
 }
 
 }  // namespace rsm
--- tasks/all-tasks/4-rsm-multipaxos/rsm/replica/store/log.hpp
+++ tasks/all-tasks/4-rsm-multipaxos/rsm/replica/store/log.hpp
@@ -3,7 +3,7 @@
 #include <rsm/replica/store/log_entry.hpp>
 
 #include <persist/fs/path.hpp>
-#include <persist/rsm/log/log.hpp>
+#include <persist/rsm/multipaxos/log/log.hpp>
 
 #include <memory>
 #include <optional>
@@ -16,7 +16,7 @@ namespace rsm {
 
 class Log {
  public:
-  explicit Log(const persist::fs::Path& dir);
+  explicit Log(const persist::fs::Path& store_dir);
 
   // One-shot
   void Open();
@@ -24,15 +24,17 @@ class Log {
   bool IsEmpty(size_t index) const;
   std::optional<LogEntry> Read(size_t index) const;
 
-  void Update(size_t index, LogEntry entry);
+  void Update(size_t index, const LogEntry& entry);
 
   void TruncatePrefix(size_t index);
 
  private:
-  std::shared_ptr<persist::rsm::ILog> MakeLogImpl(const persist::fs::Path& dir);
+  using ILogImpl = persist::rsm::multipaxos::IRandomAccessLog;
+
+  std::shared_ptr<ILogImpl> MakeLogImpl(const persist::fs::Path& store_dir);
 
  private:
-  std::shared_ptr<persist::rsm::ILog> impl_;
+  std::shared_ptr<ILogImpl> impl_;
 };
 
 }  // namespace rsm
